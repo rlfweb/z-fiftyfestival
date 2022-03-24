390bf8d8e2f89652bdbea9730a3b9dea117eb45d
REBECCA FRENCH <rlfweb8@gmail.com>
2022-03-24 12:31:45 +0000

    Deleting unwanted

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -14,22 +14,11 @@
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			fifty_festival_posted_on();
-			fifty_festival_posted_by();
-			?>
-		</div><!-- .entry-meta -->
+
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<?php fifty_festival_post_thumbnail(); ?>
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php fifty_festival_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
--- template-parts/content.php
+++ template-parts/content.php
@@ -20,12 +20,7 @@
 
 		if ( 'post' === get_post_type() ) :
 			?>
-			<div class="entry-meta">
-				<?php
-				fifty_festival_posted_on();
-				fifty_festival_posted_by();
-				?>
-			</div><!-- .entry-meta -->
+	
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
@@ -47,17 +42,8 @@
 				wp_kses_post( get_the_title() )
 			)
 		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fifty-festival' ),
-				'after'  => '</div>',
-			)
-		);
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php fifty_festival_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+
 </article><!-- #post-<?php the_ID(); ?> -->
